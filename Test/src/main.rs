445c774bfe220864b57665f2fdcd441d6c611c44
blithezhang <blitheshuo@gmail.com>
2025-05-14 13:06:04 -0400

    UPDATE leetcode project

--- Test/src/main.rs
+++ Test/src/main.rs
@@ -1,27 +1,13 @@
-use core::num;
-
 pub struct Solution;
-// impl Solution {
+
+//impl Solution {
 //     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-//         let mut left = 0;
-//         let mut right = nums.len() - 1;
-//
-//         while left <= right {
-//             let mid = left + (right - left) / 2;
-//             let mid_val = nums[mid as usize];
-//
-//             if mid_val == target {
-//                 return mid as i32;
-//             } else if mid_val < target {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
-//
-//         -1
-//     }
+//         match nums.binary_search(&target) {
+//             Ok(v) => return v.try_into().unwrap(),
+//             Err(e) => return -1, };
 // }
+// }
+
 // impl Solution {
 //     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
 //         let mut left = 0;
@@ -36,23 +22,42 @@ pub struct Solution;
 //             } else if mid_val < target {
 //                 left = mid + 1;
 //             } else {
-//                 // if mid == 0 {
-//                 //  break;
 //                 right = mid - 1;
-//             } // 防止 mid - 1 underflow
+//             }
 //         }
 //         -1
 //     }
 // }
+
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        match nums.binary_search(&target) {
-            Ok(v) => return v.try_into().unwrap(),
-            Err(e) => return -1,
-        };
+        let (mut left, mut right) = (0, nums.len());
+
+        while left < right {
+            let mid = left + (right - left) / 2;
+
+            if nums[mid] < target {
+                left = mid + 1;
+            } else if nums[mid] == target {
+                return mid as i32;
+            } else {
+                right = mid;
+            }
+        }
+        -1
     }
 }
+
+// impl Solution {
+//     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+//         match nums.binary_search(&target) {
+//             Ok(v) => v.try_into().unwrap(),
+//             Err(e) => -1,
+//         }
+//     }
+// }
 fn main() {
-    let res = Solution::search(vec![5], -5);
+    let res = Solution::search(vec![-5, 2, 4], 4);
     println!("{res}");
+    println!("{}", Solution::search(vec![-5, 2, 4], -5));
 }
