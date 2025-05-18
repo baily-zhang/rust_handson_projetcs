0aa9a4a8052907b5bc0173d698cb9aad6865e85d
blithezhang <blitheshuo@gmail.com>
2025-05-18 11:53:42 -0400

    ADDED AXUM directory

---
+++ todo_axum/target/debug/deps/todo_axum-011240ce757308ab.d
@@ -0,0 +1,5 @@
+/Users/blithe/Desktop/Code/rust/projects/todo_axum/target/debug/deps/libtodo_axum-011240ce757308ab.rmeta: src/main.rs
+
+/Users/blithe/Desktop/Code/rust/projects/todo_axum/target/debug/deps/todo_axum-011240ce757308ab.d: src/main.rs
+
+src/main.rs:
---
+++ todo_axum/target/debug/todo_axum.d
@@ -0,0 +1,1 @@
+/Users/blithe/Desktop/Code/rust/projects/todo_axum/target/debug/todo_axum: /Users/blithe/Desktop/Code/rust/projects/todo_axum/src/main.rs
