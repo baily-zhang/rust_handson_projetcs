c24d4c4bf3f13cf84567ea6e32bb1375a276b612
blithezhang <blitheshuo@gmail.com>
2025-05-19 11:07:03 -0400

    UPDATED track rules

--- Leetcodes/binary_search_704/target/debug/binary_search_704.d
+++
@@ -1,1 +0,0 @@
-/Users/blithe/Desktop/Code/rust/projects/Leetcodes/binary_search_704/target/debug/binary_search_704: /Users/blithe/Desktop/Code/rust/projects/Leetcodes/binary_search_704/src/main.rs
--- Leetcodes/binary_search_704/target/debug/deps/binary_search_704-8919bf36dbefa7c3.d
+++
@@ -1,5 +0,0 @@
-/Users/blithe/Desktop/Code/rust/projects/Leetcodes/binary_search_704/target/debug/deps/libbinary_search_704-8919bf36dbefa7c3.rmeta: src/main.rs
-
-/Users/blithe/Desktop/Code/rust/projects/Leetcodes/binary_search_704/target/debug/deps/binary_search_704-8919bf36dbefa7c3.d: src/main.rs
-
-src/main.rs:
