3a0860efa0a5ccab9879c232bbf3052fecfab3fb
Baily <blitheshuo@gmail.com>
2025-06-04 00:08:12 -0400

    Fix compilation issue in ipsniffer

--- ipsniffer/src/main.rs
+++ ipsniffer/src/main.rs
@@ -1,5 +1,8 @@
-std::env;
+use std::env;
 
+/// Simple program that prints all environment variables.
 fn main() {
-    
-}:
+    for (key, value) in env::vars() {
+        println!("{key}={value}");
+    }
+}
